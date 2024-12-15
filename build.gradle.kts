19ec5c94844b3a678af5ec2f20d461d2ad700fab
christyansegecs <christyansegecs@hotmail.com>
2024-12-15 14:29:45 -0300

    fix IOS build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.android.tools.build:gradle:8.2")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
