a358e73bd11f359ea4d118e7923014ff3aaf2d9f
christyansegecs <christyansegecs@hotmail.com>
2024-12-15 13:15:52 -0300

    fix DatabaseDriverFactory.kt for IOS build

--- shared/src/iosMain/kotlin/com/example/noteappkmm/data/local/DatabaseDriverFactory.kt
+++ shared/src/iosMain/kotlin/com/example/noteappkmm/data/local/DatabaseDriverFactory.kt
@@ -4,7 +4,7 @@ import com.example.noteappkm.database.NoteDatabase
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-class DatabaseDriverFactory {
+actual class DatabaseDriverFactory {
     actual fun createDriver(): SqlDriver {
         return NativeSqliteDriver(NoteDatabase.Schema, "note.db")
     }
